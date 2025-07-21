e5395bda039859aa598d141f9c34afd7393c9d9c
Gabriel Valadares <silva44gabriel@gmail.com>
2025-07-21 18:13:23 -0300

    fix: adiciona autowired e construtor default

--- screensound/src/main/java/br/com/valadares/screensound/ScreensoundApplication.java
+++ screensound/src/main/java/br/com/valadares/screensound/ScreensoundApplication.java
@@ -2,12 +2,15 @@ package br.com.valadares.screensound;
 
 import br.com.valadares.screensound.principal.Principal;
 import br.com.valadares.screensound.repository.ArtistaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreensoundApplication implements CommandLineRunner {
+
+	@Autowired
 	private ArtistaRepository repositorio;
 
 	public static void main(String[] args) {
--- screensound/src/main/java/br/com/valadares/screensound/model/Artista.java
+++ screensound/src/main/java/br/com/valadares/screensound/model/Artista.java
@@ -22,6 +22,8 @@ public class Artista {
     @OneToMany(mappedBy = "artista")
     private List<Musica> musicas = new ArrayList<>();
 
+    public Artista() {}
+
     public Artista(String nome, TipoArtista tipoArtista) {
         this.nome = nome;
         this.tipo = tipoArtista;
